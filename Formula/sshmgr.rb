dd04c960d839a8e2100601b024191284cbbb013a
PyGuy <pyguysprogramming2012@gmail.com>
2026-04-20 14:30:04 +0200

    commit

--- Formula/sshmgr.rb
+++ Formula/sshmgr.rb
@@ -3,18 +3,17 @@ class Sshmgr < Formula
   homepage "https://github.com/PyGuy-Programming/sshmgr"
   url "https://github.com/PyGuy-Programming/sshmgr/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "4d71475068f314665101253368b61ce54e918943b499f3e61229c345b1f747c1"
-  license "MIT" # oder was auch immer du verwendest
+  license "MIT"
 
   depends_on "fzf"
+  depends_on "nano"
 
   def install
     bin.install "sshmgr.sh" => "sshmgr"
-    
-    # known_hosts.save wird beim ersten Start erstellt, nicht beim Install
   end
 
   def post_install
-    hosts_dir = var/"sshmgr"
+    hosts_dir = Pathname.new(ENV["HOME"])/".config/sshmgr"
     hosts_dir.mkpath
     hosts_file = hosts_dir/"known_hosts.save"
     unless hosts_file.exist?
