6c7057306413609912a826c5b16a9518a0ea1080
PyGuy-Programming <pyguysprogramming2012@gmail.com>
2026-08-17 14:49:10 +0200

    updated Formula sshmgr.rb for version 2.0

--- Formula/sshmgr.rb
+++ Formula/sshmgr.rb
@@ -1,12 +1,12 @@
 class Sshmgr < Formula
   desc "A bash based SSH host manager for managing SSH connections"
   homepage "https://github.com/PyGuy-Programming/sshmgr"
-  url "https://github.com/PyGuy-Programming/sshmgr/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "afc6f055307abc8aa03713020cfb4a21eee747ba8fecfe888a7d962f1453ab65"
+  url "https://github.com/PyGuy-Programming/sshmgr/archive/refs/tags/v2.0.tar.gz"
+  sha256 "e0efda1a6632932caa437abe3d01650e210a550b68f2f96765448e4122b48c2a"
   license "MIT"
 
   depends_on "fzf"
-  depends_on "nano"
+  depends_on "jq"
 
   def install
     bin.install "sshmgr.sh" => "sshmgr"
@@ -15,11 +15,14 @@ class Sshmgr < Formula
   def post_install
     hosts_dir = Pathname.new(ENV["HOME"])/".config/sshmgr"
     hosts_dir.mkpath
-    hosts_file = hosts_dir/"known_hosts.save"
+    hosts_file = hosts_dir/"known_hosts.json"
     unless hosts_file.exist?
       hosts_file.write <<~EOS
-        # list of known hosts
-        # layout: <username>@<address> - <connection name>
+      {
+        "hosts": [
+          {"name": "example", "host": "127.0.0.1", "user": "user", "port": 22}
+        ]
+      }
       EOS
     end
   end
