ac3cbbd407c01d66a74fab8683209a9c393cf717
PyGuy-Programming <pyguysprogramming2012@gmail.com>
2026-08-17 15:21:43 +0200

    commit

--- Formula/sshmgr.rb
+++ Formula/sshmgr.rb
@@ -1,13 +1,12 @@
 class Sshmgr < Formula
   desc "A bash based SSH host manager for managing SSH connections"
   homepage "https://github.com/PyGuy-Programming/sshmgr"
-  url "https://github.com/PyGuy-Programming/sshmgr/archive/refs/tags/v2.0.tar.gz"
-  sha256 "e0efda1a6632932caa437abe3d01650e210a550b68f2f96765448e4122b48c2a"
+  url "https://github.com/PyGuy-Programming/sshmgr/archive/refs/tags/v2.1.tar.gz"
+  sha256 "6647b1034a8b59d9b2c3b5391700d833815b247dbade19dc016b045bd43b0fd5"
   license "MIT"
 
   depends_on "fzf"
   depends_on "jq"
-
   def install
     bin.install "sshmgr.sh" => "sshmgr"
   end
